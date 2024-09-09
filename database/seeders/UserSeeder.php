715f4d45c4141f4c4c21c262d6bf5a5d4e1cab07
Alan Diaz <alandiaz719@gmail.com>
2024-09-09 16:32:56 -0400

    paginas

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,61 +15,66 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('handyman_users_app')->insert([
-            'name' => 'Francisco Noceda',
-            'email' => 'admin@demo.com',
-            'short_description' => 'short_description',
-            'qualification' => 4.0,
-            'profession' => 'profession',
-            'price' => 24.00,
-            'profile_photo_path' => 'worker1.png',
-            'password' => bcrypt('secret')
-        ]);
+        // DB::table('handyman_users_app')->insert([
+        //     'name' => 'Francisco Noceda',
+        //     'email' => 'admin@demo.com',
+        //     'short_description' => 'short_description',
+        //     'qualification' => 4.0,
+        //     'profession' => 'profession',
+        //     'price' => 24.00,
+        //     'profile_photo_path' => 'worker1.png',
+        //     'password' => bcrypt('secret')
+        // ]);
+        $item=2;
+        for ($i=0; $i < 100; $i++) { 
+            DB::table('handyman_users_app')->insert([
+                'name' => 'Cajero'.$item,
+                'email' => 'caja'.$item.'@demo.com',
+                'short_description' => 'short_description',
+                'qualification' => 4.0,
+                'profession' => 'profession',
+                'price' => 24.00,
+                'profile_photo_path' => 'worker2.png',
+                'password' => bcrypt('secret'),
+            ]);
+            $item++;
+        }
+           
+        
+       
 
+        // DB::table('handyman_users_app')->insert([
+        //     'name' => 'Alexandra Vera',
+        //     'email' => 'ale@demo.com',
+        //     'short_description' => 'short_description',
+        //     'qualification' => 4.0,
+        //     'profession' => 'profession',
+        //     'price' => 24.00,
+        //     'profile_photo_path' => 'worker3.png',
+        //     'password' => bcrypt('secret'),
+        // ]);
 
-        DB::table('handyman_users_app')->insert([
-            'name' => 'Cajero1',
-            'email' => 'caja@demo.com',
-            'short_description' => 'short_description',
-            'qualification' => 4.0,
-            'profession' => 'profession',
-            'price' => 24.00,
-            'profile_photo_path' => 'worker2.png',
-            'password' => bcrypt('secret'),
-        ]);
+        // DB::table('handyman_users_app')->insert([
+        //     'name' => 'Monica Ortiz',
+        //     'email' => 'monica@demo.com',
+        //     'short_description' => 'short_description',
+        //     'qualification' => 4.0,
+        //     'profession' => 'profession',
+        //     'price' => 24.00,
+        //     'profile_photo_path' => 'worker1.png',
+        //     'password' => bcrypt('secret'),
+        // ]);
 
-        DB::table('handyman_users_app')->insert([
-            'name' => 'Alexandra Vera',
-            'email' => 'ale@demo.com',
-            'short_description' => 'short_description',
-            'qualification' => 4.0,
-            'profession' => 'profession',
-            'price' => 24.00,
-            'profile_photo_path' => 'worker3.png',
-            'password' => bcrypt('secret'),
-        ]);
-
-        DB::table('handyman_users_app')->insert([
-            'name' => 'Monica Ortiz',
-            'email' => 'monica@demo.com',
-            'short_description' => 'short_description',
-            'qualification' => 4.0,
-            'profession' => 'profession',
-            'price' => 24.00,
-            'profile_photo_path' => 'worker1.png',
-            'password' => bcrypt('secret'),
-        ]);
-
-        DB::table('handyman_users_app')->insert([
-            'name' => 'Thalia Benitez',
-            'email' => 'thalia@demo.com',
-            'short_description' => 'short_description',
-            'qualification' => 4.0,
-            'profession' => 'profession',
-            'price' => 24.00,
-            'profile_photo_path' => 'worker1.png',
-            'password' => bcrypt('secret'),
-        ]);
+        // DB::table('handyman_users_app')->insert([
+        //     'name' => 'Thalia Benitez',
+        //     'email' => 'thalia@demo.com',
+        //     'short_description' => 'short_description',
+        //     'qualification' => 4.0,
+        //     'profession' => 'profession',
+        //     'price' => 24.00,
+        //     'profile_photo_path' => 'worker1.png',
+        //     'password' => bcrypt('secret'),
+        // ]);
 
 
     }
